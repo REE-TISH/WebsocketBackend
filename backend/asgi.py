e74ee96a9869662ce6bf8fa99371dc63b9b9975e
REE_TISH <reetishk798@gmail.com>
2025-08-02 15:51:54 +0530

    Set Debug

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,13 +1,14 @@
 import os
 import django
-from channels.routing import get_default_application,ProtocolTypeRouter,URLRouter
+from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
 import PleaseWork.routing
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
 application = ProtocolTypeRouter({
-    "http":get_default_application(),
+    "http":get_asgi_application(),
     "websocket":AuthMiddlewareStack(
         URLRouter(
             PleaseWork.routing.websocket_urlpatterns
